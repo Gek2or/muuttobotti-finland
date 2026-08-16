faca3fe87f26b2297969c6a381e8cf0398c3a5d4
Gek2or <74296171+Gek2or@users.noreply.github.com>
2026-08-16 20:38:24 +0300

    Remove legacy app from homepage runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import MuuttobottiApp from "./MuuttobottiApp";
 import V11NativeExperience from "./V11NativeExperience";
-import PricingGuard from "./PricingGuard";
-import MovePlanBridge from "./MovePlanBridge";
+import NativeFunctionalShell from "./NativeFunctionalShell";
 
 export default function Home() {
   return (
     <>
       <V11NativeExperience />
-      <MuuttobottiApp />
-      <PricingGuard />
-      <MovePlanBridge />
+      <NativeFunctionalShell />
     </>
   );
 }
