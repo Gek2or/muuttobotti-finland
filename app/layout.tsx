d3f77b8d335e91cbf4ac2aefcbc4e4f604b793ca
Gek2or <74296171+Gek2or@users.noreply.github.com>
2026-08-16 21:04:25 +0300

    Load V11 UX polish layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./overrides.css";
 import "./v11-native.css";
 import "./v11-credibility.css";
 import "./v11-plan.css";
+import "./v11-ux.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin", "cyrillic"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin", "cyrillic"] });
