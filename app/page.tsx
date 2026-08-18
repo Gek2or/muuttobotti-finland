ddda0c420e2da98740ba8be5a30c43a7f455a561
Stanislav Kosytskyy <74296171+Gek2or@users.noreply.github.com>
2026-08-19 00:19:55 +0300

    Switch homepage to rebuilt opening experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import V11NativeExperience from "./V11NativeExperience";
+import V11HeroReset from "./V11HeroReset";
 import NativeFunctionalShell from "./NativeFunctionalShell";
 import ProductionLegalStrip from "./ProductionLegalStrip";
 
 export default function Home() {
   return (
     <>
-      <V11NativeExperience />
+      <V11HeroReset />
       <NativeFunctionalShell />
       <ProductionLegalStrip />
     </>
