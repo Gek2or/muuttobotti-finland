211e735fbc9c0a9c8dc3fece30806940ade00e7b
Stanislav Kosytskyy <74296171+Gek2or@users.noreply.github.com>
2026-08-18 22:47:54 +0300

    Finish homepage with production legal strip

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import V11NativeExperience from "./V11NativeExperience";
 import NativeFunctionalShell from "./NativeFunctionalShell";
+import ProductionLegalStrip from "./ProductionLegalStrip";
 
 export default function Home() {
   return (
     <>
       <V11NativeExperience />
       <NativeFunctionalShell />
+      <ProductionLegalStrip />
     </>
   );
 }
