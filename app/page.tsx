e1e3df3438343177b6da4494e9f5c3e3d456aa26
Stanislav Kosytskyy <74296171+Gek2or@users.noreply.github.com>
2026-08-24 17:02:38 +0300

    Mount booking runtime controller

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import MuuttobottiArtV3 from "./MuuttobottiArtV3";
 import FaqPortal from "./FaqPortal";
 import LocalizedSurfaceFixes from "./LocalizedSurfaceFixes";
 import BusinessCalculatorV2 from "./BusinessCalculatorV2";
+import BookingRuntimeController from "./BookingRuntimeController";
 import VisualMotionEnhancer from "./VisualMotionEnhancer";
 import { faqContent } from "./faq-content";
 import "./experience-v5.css";
 import "./business-calculator-v2.css";
+import "./booking-runtime.css";
 
 export default function Home() {
   const faqSchema = {
@@ -28,6 +30,7 @@ export default function Home() {
       <FaqPortal />
       <LocalizedSurfaceFixes />
       <BusinessCalculatorV2 />
+      <BookingRuntimeController />
       <VisualMotionEnhancer />
     </>
   );
