72f748d3e98ecb734e9a34d3ffb78de15ec99c6d
Stanislav Kosytskyy <74296171+Gek2or@users.noreply.github.com>
2026-08-24 13:39:18 +0300

    Add FAQ structured data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
 import MuuttobottiArtV3 from "./MuuttobottiArtV3";
 import FaqPortal from "./FaqPortal";
+import { faqContent } from "./faq-content";
 
 export default function Home() {
+  const faqSchema = {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    mainEntity: faqContent.fi.map(([question, answer]) => ({
+      "@type": "Question",
+      name: question,
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: answer,
+      },
+    })),
+  };
+
   return (
     <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }} />
       <MuuttobottiArtV3 />
       <FaqPortal />
     </>
