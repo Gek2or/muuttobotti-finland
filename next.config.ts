1018d5b924e5b8e814b59e1e5fffc2efed1fd7c1
Stanislav Kosytskyy <74296171+Gek2or@users.noreply.github.com>
2026-09-13 22:48:55 -0700

    Redirect legacy Järvenpää moving URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/moving-jarvenpää",
+        destination: "/moving-jarvenpaa",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
